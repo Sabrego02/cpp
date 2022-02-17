c309954372df5722093d505678a169aa1e969b78
Sandra Abrego <sandra32.abrego@gmail.com>
2022-02-16 21:46:22 -0600

    Agregando Ejercicio19

---
+++ Ejercicios/Ejercicio19_Variantes_Ciclosfor/Ciclos_for.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int i =0; i < 10; i++)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    cout << endl;
+
+    for (int i = 9; 1 >= 0; i--)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    cout << endl;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << i << " ";
+        if (i == 115)
+        {
+            break;
+        }
+    }
+   
+    return 0;
+}
