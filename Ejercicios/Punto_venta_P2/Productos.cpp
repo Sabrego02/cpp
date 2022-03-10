3e470e9c3c7e5b612bbd08c915f6c9b35282adba
Sandra Abrego <sandra32.abrego@gmail.com>
2022-03-10 17:24:29 -0600

    Agregando ejercicios

---
+++ Ejercicios/Punto_venta_P2/Productos.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void productos (int opcion)
+{
+    system ("cls");
+
+    switch (opcion)
+    {
+    case 1:
+    {
+        cout << " Bebidas Calientes" << endl;
+        cout <<" ******************" << endl;
+        system("pause");
+        break;
+    }
+    case 2:
+    {
+        cout << " Bebidas Frias" << endl;
+        cout <<" *****************" << endl;
+        system("pause");
+        break;
+    }
+    case 3:
+    {
+        cout << " Reposteria" << endl;
+        cout <<" *****************" << endl;
+        system("pause");
+        break;
+    }
+        
+    
+    default:
+        break;
+    }
+}
