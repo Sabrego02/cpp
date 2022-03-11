1ef924d7ac337ebcae83d3e575f2dc7bc7909c98
Sandra Abrego <sandra32.abrego@gmail.com>
2022-03-10 20:53:48 -0600

    Agregando Tarea 6

---
+++ Ejercicios/Punto_venta_P4/Productos.cpp
@@ -0,0 +1,67 @@
+ #include <iostream>
+
+using namespace std;
+
+extern void agregarProducto(string descripcion, int cantidad, double precio);
+
+void productos (int opcion)
+{
+    system ("cls");
+
+int opcionProducto=0;
+    switch (opcion)
+    {
+    case 1:
+    {
+       
+        cout << " Bebidas Calientes" << endl;
+        cout <<" ******************" << endl;
+        cout << " 1- Capuccino" << endl;
+        cout <<" 2- Expressa" << endl;
+
+        cin >> opcionProducto;
+
+        switch (opcionProducto)
+        {
+        case 1:
+            agregarProducto( "Capuccino", 1, 40);
+            break;  
+         case 2:
+            agregarProducto( "Expresso", 1, 30);
+            break; 
+        default:
+            {
+                cout << "Opcion no valida";
+                return;
+                break;
+
+            }
+            
+        }
+        
+        cout << endl;
+        cout << "Producto agregado"<< endl;
+        system("pause");
+        break;
+    }
+
+    case 2:
+    {
+        cout << " Bebidas Frias" << endl;
+        cout <<" *****************" << endl;
+        system("pause");
+        break;
+    }
+    case 3:
+    {
+        cout << " Reposteria" << endl;
+        cout <<" *****************" << endl;
+        system("pause");
+        break;
+    }
+        
+    
+    default:
+        break;
+    }
+}
